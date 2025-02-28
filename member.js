8afaf15ddd462a777a15dfaf21176a03fea093e0
Joe Sylnice <nerdroid23@gmail.com>
2025-02-28 01:45:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    // Skills: member.js
+    var skills = [
+        {
+            "name": "Javascript",
+            "level": "Intermediate"
+        },
+        {
+            "name": "NodeJS",
+            "level": "Intermediate"
+        },
+        {
+            "name": "React",
+            "level": "Intermediate"
+        },
+        {
+            "name": "Angular",
+            "level": "Intermediate"
+        }
+    ];
+    return skills;
+}
